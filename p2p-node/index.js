a9eb1753aede93c2ae2af4759944398549d03a7a
wirthdevelop <120033291+wirthdevelop@users.noreply.github.com>
2025-06-06 14:03:21 +0000

    ADD Code Base from Step 2 in Step 3

--- p2p-node/index.js
+++ p2p-node/index.js
@@ -1,1 +1,32 @@
-console.log("Hello World");
+import { createLibp2p } from 'libp2p'
+import { tcp } from '@libp2p/tcp'
+import { noise } from '@chainsafe/libp2p-noise'
+import { yamux } from '@chainsafe/libp2p-yamux'
+
+const main = async () => {
+  const node = await createLibp2p({
+    addresses: {
+      // add a listen address (localhost) to accept TCP connections on a random port
+      listen: ['/ip4/127.0.0.1/tcp/0']
+    },
+    transports: [tcp()],
+    connectionEncrypters: [noise()],
+    streamMuxers: [yamux()]
+  })
+
+  // start libp2p
+  await node.start()
+  console.log('libp2p has started')
+
+  // print out listening addresses
+  console.log('listening on addresses:')
+  node.getMultiaddrs().forEach((addr) => {
+    console.log(addr.toString())
+  })
+
+  // stop libp2p
+  await node.stop()
+  console.log('libp2p has stopped')
+}
+
+main().then().catch(console.error)
